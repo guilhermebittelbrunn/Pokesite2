18edfebf275aff23839881c6230f0fd056c50176
Guilherme Bittelbrunn <guilherme.bitte@unifebe.edu.br>
2023-05-11 21:33:57 -0300

    add new Project
    Pokesite, v2.0, created with vite js, add tailwind css and genereted with yarn

---
+++ pokesite/src/components/Nav.jsx
@@ -0,0 +1,14 @@
+export default function Nav(){
+    return(
+        <>
+            <nav>
+                <div>
+                    <div className="left-nav">
+                        <h3>PokeSite</h3>
+                    </div>
+                    <div className="right-nav"></div>
+                </div>
+            </nav>
+        </>
+    )
+}
